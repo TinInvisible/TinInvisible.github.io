072fa1114b4f6957fcfcc5b3628c8ab0b37ba602
TinInvisible <botomtinlon@gmail.com>
2022-11-30 14:11:41 +0700

    Adding code

---
+++ mvc-admin/components/products/Data.js
@@ -0,0 +1,85 @@
+const imgSrc = [
+    {
+        page: 'billing',
+        logo: '../assets/img/logo-ct.png',
+        team_2: '../assets/img/team-2.jpg',
+        logo_spotify: '../assets/img/small-logos/logo-spotify.svg',
+        pattern_tree: '../assets/img/illustrations/pattern-tree.svg',
+        master_card: '../assets/img/logos/mastercard.png',
+        visa: '../assets/img/logos/visa.png'
+    },
+    {
+        page: 'dashboard',
+        logo: '../assets/img/logo-ct.png',
+        team_1: '../assets/img/team-1.jpg',
+        team_2: '../assets/img/team-2.jpg',
+        team_3: '../assets/img/team-3.jpg',
+        team_4: '../assets/img/team-4.jpg',
+        logo_spotify: '../assets/img/small-logos/logo-spotify.svg',
+        logo_xd: '../assets/img/small-logos/logo-xd.svg',
+        logo_atlassian: '../assets/img/small-logos/logo-atlassian.svg',
+        logo_slack: '../assets/img/small-logos/logo-slack.svg',
+        logo_jira: '../assets/img/small-logos/logo-jira.svg',
+        logo_invision: '../assets/img/small-logos/logo-invision.svg',
+    },
+    {
+        page: 'home-page',
+        logo:"../assets/img/logo-ct.png",
+        team_2: "../assets/img/team-2.jpg",
+        logo_spotify: "../assets/img/small-logos/logo-spotify.svg",
+        us:"../assets/img/icons/flags/US.png",
+        de:"../assets/img/icons/flags/DE.png",
+        gb:"../assets/img/icons/flags/GB.png",
+        br:"../assets/img/icons/flags/BR.png"
+
+    },
+    {
+        page: 'notifications',
+        logo:"../assets/img/logo-ct.png",
+        team_2:"../assets/img/team-2.jpg",
+        logo_spotify:"../assets/img/small-logos/logo-spotify.svg"
+
+    },
+    {
+        page: 'profile',
+        logo:"../assets/img/logo-ct.png",
+        team_2: "../assets/img/team-2.jpg",
+        logo_spotify: "../assets/img/small-logos/logo-spotify.svg",
+        bruce_mars:"../assets/img/bruce-mars.jpg",
+        kal_visuals_square:"../assets/img/kal-visuals-square.jpg",
+        marie:"../assets/img/marie.jpg",
+        ivana_square:"../assets/img/ivana-square.jpg",
+        team_4:"../assets/img/team-4.jpg",
+        team_3: "../assets/img/team-3.jpg",
+        home_decor_1:"../assets/img/home-decor-1.jpg",
+        team_1:"../assets/img/team-1.jpg",
+        home_decor_2:"../assets/img/home-decor-2.jpg",
+        home_decor_3:"../assets/img/home-decor-3.jpg",
+        photo:"https://images.unsplash.com/photo-1606744824163-985d376605aa?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80"
+
+
+    },
+    {
+        page: 'sign-in'
+    },
+    {
+        page: 'sign-up'
+    },
+    {
+        page: 'tables',
+        logo:"../assets/img/logo-ct.png",
+        team_2: "../assets/img/team-2.jpg",
+        logo_spotify: "../assets/img/small-logos/logo-spotify.svg",
+        team_3: "../assets/img/team-3.jpg",
+        team_4: "../assets/img/team-4.jpg",
+        asana: "../assets/img/small-logos/logo-asana.svg",
+        github:"../assets/img/small-logos/github.svg",
+        atlassian:"../assets/img/small-logos/logo-atlassian.svg",
+        bootstrap:"../assets/img/small-logos/bootstrap.svg",
+        slack:"../assets/img/small-logos/logo-slack.svg",
+        detvo:"../assets/img/small-logos/devto.svg",
+
+    }
+]
+
+exports.imgSrc = imgSrc;
